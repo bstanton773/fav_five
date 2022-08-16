665a95bf5b8b03660430afb4c5b925acd03d47e5
Brian <bstanton773@gmail.com>
2022-08-16 10:33:33 -0500

    Add base layout and index html

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
 
 
 @app.route('/favorites')
